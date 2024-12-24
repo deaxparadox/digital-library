f901b1185eea231bf182d4bbf09b1a57e6be8835
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2024-12-24 23:15:07 +0530

    removed navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono ,Inter} from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({subsets:["latin"]});
 
@@ -28,10 +27,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <div className="relative w-full flex items-center justify-center">
-          <Navbar />
-        </div>
-        
         {children}
       </body>
     </html>
